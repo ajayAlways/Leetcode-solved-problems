1124e51fe443af8d2a3f8e3f6ff43bc89191e918
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-12 12:56:10 +0530

    Time: 156 ms (16.74%), Space: 59 MB (37.83%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -3,10 +3,10 @@ public:
     int maxArea(vector<int>& height) {
         int ans=0,i=0,j=height.size()-1;
         while(i<j){
-            int mini = min(height[i],height[j])*(j-i);
-            ans = max(ans, mini);
-            if(height[i]<height[j]) i++;
-            else j--;
+            int mini = min(height[i],height[j]);
+            ans = max(ans, mini*(j-i));
+            while(height[i]<=mini && i<j) i++;
+            while(height[j]<=mini && i<j) j--;
         }
         return ans;
     }
