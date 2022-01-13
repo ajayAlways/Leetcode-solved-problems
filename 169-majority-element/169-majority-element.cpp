a3e907df8cd195800648722a459e963c48f1664f
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-13 11:30:15 +0530

    Time: 31 ms (20.63%), Space: 19.6 MB (50.63%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>m;
+        // unordered_map<int,int>m;
+        int count=0,majarity=nums[0];
         for(auto i:nums){
-            m[i]++;
-            if(m[i]>(nums.size()/2)) return i;
+            // m[i]++;
+            // if(m[i]>(nums.size()/2)) return i;
+            if(count==0){
+                majarity = i;
+            }
+            count+=(i==majarity)?1:-1;
         }
-        return 0;
+        return majarity;
     }
 };
