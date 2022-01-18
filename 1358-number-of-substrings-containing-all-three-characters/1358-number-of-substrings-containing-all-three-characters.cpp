1477a46036533fcc95435b748495b05326b80346
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-18 21:14:37 +0530

    Time: 10 ms (99.26%), Space: 8.5 MB (85.40%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int>v(3,0);
-        int j=0,count=0;
+        //  Sliding Window Solution        
+        // vector<int>v(3,0);
+        // int j=0,count=0;
+        // for(int i=0;i<s.length();i++){
+        //     v[s[i]-'a']++;
+        //     while(v[0]>0 && v[1]>0 && v[2]>0){
+        //         v[s[j]-'a']--;
+        //         j++;
+        //     }
+        //     count+=j;
+        // }
+        
+        // without sliding window
+        int a=-1,b=-1,c=-1,count=0;
         for(int i=0;i<s.length();i++){
-            v[s[i]-'a']++;
-            while(v[0]>0 && v[1]>0 && v[2]>0){
-                v[s[j]-'a']--;
-                j++;
-            }
-            count+=j;
+            if(s[i]=='a') a=i;
+            else if(s[i]=='b') b=i;
+            else c=i;
+            count+=min(a,min(b,c))+1;
         }
         return count;
     }
