52b44f1fca6d71517c1ebabf27690b0447f9fe4b
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-19 16:32:04 +0530

    Time: 3 ms (83.17%), Space: 6.4 MB (99.73%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>m(26,0),ans;
+        for(int i=0;i<s.length();i++){
+            m[s[i]-'a'] = i; 
+        }
+        int j=0,anchor=0;
+        for(int i=0;i<s.length();i++){
+            j = max(j,m[s[i]-'a']);
+            if(i==j){
+                ans.push_back(i-anchor+1);
+                anchor=i+1;
+            }
+        }
+        return ans;
+    }
+};
