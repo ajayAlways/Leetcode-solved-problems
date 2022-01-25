5c061e7ec200a2669acc5ef7510e2fac095f58a3
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-26 01:05:48 +0530

    Time: 1709 ms (10.26%), Space: 294.8 MB (25.17%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -13,17 +13,20 @@ public:
     ListNode* deleteMiddle(ListNode* head) {
         if(head->next==NULL) return NULL;
         int count=0;
-        ListNode *head1 = head;
-        while(head1!=NULL){
-            count++;
-            head1 = head1->next;
+        // ListNode *head1 = head;
+        ListNode *fast = head,*slow=head,*dummy = new ListNode(0);
+        dummy->next = head;
+        slow=dummy;
+        while(fast!=NULL &&  fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        head1 = head;
-        for(int i=0;i<count/2 -1;i++){
-            head1=head1->next;
-        }
-        ListNode *temp = head1->next;
-        head1->next = temp->next;
+        // head1 = head;
+        // for(int i=0;i<count/2 -1;i++){
+        //     head1=head1->next;
+        // }
+        ListNode *temp = slow->next;
+        slow->next = temp->next;
         delete(temp);
         return head;
     }
