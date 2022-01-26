f778fc43f029955024aff44fbf32f5b3255c183d
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-26 16:57:04 +0530

    Time: 40 ms (27.61%), Space: 10.5 MB (82.19%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')'){
+                if(st.empty()){
+                    s.erase(s.begin()+i);
+                    i--;
+                }
+                else st.pop();
+            }
+        }
+        while(!st.empty()){
+            s.erase(s.begin()+st.top());
+            st.pop();
+        }
+        return s;
+    }
+};
