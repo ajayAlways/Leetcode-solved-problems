0cb9b53044cd26483aee282057d746ae74e2a8ce
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-26 09:59:12 +0530

    Time: 19 ms (70.55%), Space: 16.8 MB (84.94%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int unique = 0;
-        for(auto i:nums) unique^=i;
-        return unique;
+        int ans=0;
+        for(auto i:nums){
+            ans^=i;
+        }
+        return ans;
     }
 };
