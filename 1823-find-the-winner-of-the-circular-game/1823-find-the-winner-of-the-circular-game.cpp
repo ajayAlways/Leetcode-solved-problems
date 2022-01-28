b6f42c0cbe1debfa8f1ef8d695b1d00ff4710973
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-28 10:56:35 +0530

    Time: 13 ms (42.23%), Space: 6.2 MB (55.03%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int findTheWinner(int n, int k) {
         // O(n^3)         
-        // vector<int>v(n);
-        // int pos=0;
-        // for(int i=1;i<=n;i++) v[i-1]=i;
-        // while(v.size()>1){
-        //     pos=(pos+k-1)%v.size();
-        //     v.erase(v.begin()+pos);
-        //     pos = (pos)%v.size();
-        // }
-        // return v[0];
-        queue<int>q;
-        for(int i=0;i<n;i++) q.push(i+1);
-        while(q.size()>1){
-            int x = k-1;
-            while(x--){
-                int val = q.front();
-                q.pop();
-                q.push(val);
-            }
-            q.pop();
+        vector<int>v(n);
+        int pos=0;
+        for(int i=1;i<=n;i++) v[i-1]=i;
+        while(v.size()>1){
+            pos=(pos+k-1)%v.size();
+            v.erase(v.begin()+pos);
+            pos = (pos)%v.size();
         }
-        return q.front();
+        return v[0];
+        // queue<int>q;
+        // for(int i=0;i<n;i++) q.push(i+1);
+        // while(q.size()>1){
+        //     int x = k-1;
+        //     while(x--){
+        //         int val = q.front();
+        //         q.pop();
+        //         q.push(val);
+        //     }
+        //     q.pop();
+        // }
+        // return q.front();
     }
 };
