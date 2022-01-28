9107319f7aef294e5cf7d25e7d7c1e08024ee066
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-01-28 10:54:05 +0530

    Time: 147 ms (5.91%), Space: 24.6 MB (16.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int>v(n);
-        int pos=0;
-        for(int i=1;i<=n;i++) v[i-1]=i;
-        while(v.size()>1){
-            pos=(pos+k-1)%v.size();
-            v.erase(v.begin()+pos);
-            pos = (pos)%v.size();
+        // O(n^3)         
+        // vector<int>v(n);
+        // int pos=0;
+        // for(int i=1;i<=n;i++) v[i-1]=i;
+        // while(v.size()>1){
+        //     pos=(pos+k-1)%v.size();
+        //     v.erase(v.begin()+pos);
+        //     pos = (pos)%v.size();
+        // }
+        // return v[0];
+        queue<int>q;
+        for(int i=0;i<n;i++) q.push(i+1);
+        while(q.size()>1){
+            int x = k-1;
+            while(x--){
+                int val = q.front();
+                q.pop();
+                q.push(val);
+            }
+            q.pop();
         }
-        return v[0];
+        return q.front();
     }
 };
