22a74b0a30783d64e1cbcd4698291099522906a1
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-02-11 11:19:38 +0530

    Time: 58 ms (59.06%), Space: 41.5 MB (43.37%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,22 +11,29 @@
  */
 class Solution {
 public:
+    int solve(TreeNode* root, int p, int gp){
+        if(root==NULL) return 0;
+        
+        return solve(root->left,root->val,p) + solve(root->right,root->val,p) + (gp%2 ? 0:root->val);
+    }
+    
     int sumEvenGrandparent(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        int res=0;
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-            if(node->val % 2==0){
-                if(node->left && node->left->left) res+=node->left->left->val;
-                if(node->left && node->left->right) res+=node->left->right->val;
-                if(node->right && node->right->left) res+=node->right->left->val;
-                if(node->right && node->right->right) res+=node->right->right->val;
-            }
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // int res=0;
+        // while(!q.empty()){
+        //     TreeNode* node = q.front();
+        //     q.pop();
+        //     if(node->val % 2==0){
+        //         if(node->left && node->left->left) res+=node->left->left->val;
+        //         if(node->left && node->left->right) res+=node->left->right->val;
+        //         if(node->right && node->right->left) res+=node->right->left->val;
+        //         if(node->right && node->right->right) res+=node->right->right->val;
+        //     }
+        //     if(node->left) q.push(node->left);
+        //     if(node->right) q.push(node->right);
+        // }
+        int res = solve(root,1,1);
         return res;
     }
 };
