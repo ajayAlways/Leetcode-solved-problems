cc4f7a153d7d18278b5316064aec0627a01cc7ac
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-02-21 00:35:07 +0530

    Time: 36 ms (5.23%), Space: 25.8 MB (6.33%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -10,22 +10,36 @@
  * };
  */
 class Solution {
+private:
+    int maxLevel=1;
 public:
+    void dfs(TreeNode* root,int l,vector<int> &level){
+        if(!root) return;
+        level[l-1] = max(level[l-1],root->val);
+        maxLevel = max(maxLevel,l);
+        dfs(root->left,l+1,level);        
+        dfs(root->right,l+1,level);
+    }
     vector<int> largestValues(TreeNode* root) {
         if(!root) return {};
-        queue<TreeNode*>q;
-        vector<int>res;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size(),maxVal=INT_MIN;
-            while(size--){
-                TreeNode* node = q.front();
-                q.pop();
-                maxVal = max(maxVal, node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            res.push_back(maxVal);
+        vector<int>level(10000,INT_MIN),res;
+        // queue<TreeNode*>q;
+        // vector<int>res;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size = q.size(),maxVal=INT_MIN;
+        //     while(size--){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         maxVal = max(maxVal, node->val);
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     res.push_back(maxVal);
+        // }
+        dfs(root,1,level);
+        for(int i=0;i<maxLevel;i++){
+            res.push_back(level[i]);
         }
         return res;
     }
