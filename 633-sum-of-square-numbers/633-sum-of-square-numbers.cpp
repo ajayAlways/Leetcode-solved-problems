567b69a3a7dfd4039adb8057b89b7829076f6667
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-03-09 18:50:37 +0530

    Time: 4 ms (57.48%), Space: 6 MB (9.32%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -3,12 +3,11 @@ public:
     bool judgeSquareSum(int c) {
         int j = sqrt(c);
         unordered_set<int>st;
+        
         for(int i=0;i<=j;i++){
-            st.insert(i*i);
-        }
-        for(int i=0;i<=j;i++){
-            int num = c - i*i;
-            if(st.count(num)==1) return true;
+            double num1 = sqrt(c - i*i);
+            int num2 = sqrt(c - i*i);
+            if(num1==num2) return true;
         }
         return false;
     }
