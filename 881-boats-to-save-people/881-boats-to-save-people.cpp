f0ecfa0a2017f8968d2a0daf24d777172640ecb9
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-03-24 21:36:54 +0530

    Time: 178 ms (7.24%), Space: 41.9 MB (57.42%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int res=0,i=0,j=people.size()-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                res++;
+                i++;
+                j--;
+            }
+            else{
+                res++;
+                j--;
+            }
+        }
+        return res;
+    }
+};
