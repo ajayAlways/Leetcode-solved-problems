7b14090f446d132f50eb573c29c1db27178cd42d
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-04-12 12:20:13 +0530

    Time: 3 ms (45.24%), Space: 8.9 MB (5.20%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -6,28 +6,37 @@ public:
         if(grid[i][j]==0){
             int count=0;
             for(int k=0;k<8;k++){
-                if(i+dx[k]>=0 && i+dx[k]<grid.size() && j+dy[k]>=0 && j+dy[k]<grid[0].size() && grid[i+dx[k]][j+dy[k]]==1) count++;
+                if(i+dx[k]>=0 && i+dx[k]<grid.size() && j+dy[k]>=0 && j+dy[k]<grid[0].size() && (grid[i+dx[k]][j+dy[k]]==1 || grid[i+dx[k]][j+dy[k]]==-1)) count++;
             } 
             return count==3;
         }
         else{
             int count=0;
             for(int k=0;k<8;k++){
-                if(i+dx[k]>=0 && i+dx[k]<grid.size() && j+dy[k]>=0 && j+dy[k]<grid[0].size() &&grid[i+dx[k]][j+dy[k]]==1) count++;
+                if(i+dx[k]>=0 && i+dx[k]<grid.size() && j+dy[k]>=0 && j+dy[k]<grid[0].size() && (grid[i+dx[k]][j+dy[k]]==1 || grid[i+dx[k]][j+dy[k]]==-1)) count++;
             } 
             return count<2 || count>3;
         }
         return true;
     }
     void gameOfLife(vector<vector<int>>& board) {
-        vector<pair<int,int>>pos;
+        // vector<pair<int,int>>pos;
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(countNeighbors(board,i,j)) pos.push_back({i,j});
+                if(countNeighbors(board,i,j)){
+                    if(board[i][j]==0) board[i][j]=2;
+                    else board[i][j]=-1;
+                }
             }
         }
-        for(auto& i:pos){
-            board[i.first][i.second] = !board[i.first][i.second];
+        // for(auto& i:pos){
+        //     board[i.first][i.second] = !board[i.first][i.second];
+        // }
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==1 || board[i][j]==2) board[i][j]=1;
+                else board[i][j]=0;
+            }
         }
     }
 };
