c760f07104eba873c4ed443a765eff83550df6ab
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-04-17 21:46:24 +0530

    Time: 3 ms (27.49%), Space: 5.8 MB (82.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+    
     int climbStairs(int n) {
         if(n==1) return 1;
         int a=1,b=1;
         for(int i=2;i<=n;i++){
-            int t=a;
-            a=b;
+            int t = a;
+            a = b;
             b+=t;
         }
         return b;
