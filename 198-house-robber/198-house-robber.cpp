086d6ed7edefa912f670d46189c15f5373384431
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-04-17 22:08:54 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (5.34%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
+    int t[101];
+    int solve(vector<int>nums, int n){
+        if(n<0) return 0;
+        if(t[n]!=-1) return t[n];
+        return t[n] = max(nums[n]+solve(nums,n-2),solve(nums,n-1));
+    }
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        if(nums.size()==2) return max(nums[0],nums[1]);
-        
-        vector<int>dp(nums.size());
-        
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i=2;i<dp.size();i++){
-            dp[i] = max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[nums.size()-1];
+        memset(t,-1,sizeof(t));
+        int res = solve(nums,nums.size()-1);
+        return res;
     }
 };
