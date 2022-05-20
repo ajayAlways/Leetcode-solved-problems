85222aeb9085ba0ff03c61330a6ffd4197af6346
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-05-20 17:53:03 +0530

    Time: 82 ms (23.11%), Space: 47.6 MB (12.88%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,17 +1,21 @@
 class Solution {
+private:
+    bool t[50001];
 public:
     bool solve(vector<int>& arr, int start, unordered_map<int,int>&vis){
         if(start<0 || start>=arr.size()) return false;
         vis[start]++;
         if(vis[start]>2) return false;
+        if(t[start]==true) return true;
         else{
-            if(arr[start]==0) return true;
-            return solve(arr,start+arr[start],vis) || solve(arr,start-arr[start],vis);
+            if(arr[start]==0) return t[start] = true;
+            return t[start] = solve(arr,start+arr[start],vis) || solve(arr,start-arr[start],vis);
         }
         
     }
     bool canReach(vector<int>& arr, int start) {
         unordered_map<int,int>vis;
+        memset(t,false,sizeof(t));
         return solve(arr,start,vis);
     }
 };
