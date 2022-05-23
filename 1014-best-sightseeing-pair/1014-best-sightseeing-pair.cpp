865a48a6004359d72e3862fb18357cf0f7199fec
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-05-23 22:50:35 +0530

    Time: 100 ms (15.52%), Space: 39.4 MB (52.33%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int ans=INT_MIN,leftMax=INT_MIN;
-        stack<int>st;
-        for(int i=values.size()-1;i>0;i--){
-            leftMax = max(leftMax,values[i]-i);
-            st.push(leftMax);
+        //  Time - O(N), Space - O(N)
+        // int ans=INT_MIN,leftMax=INT_MIN;
+        // stack<int>st;
+        // for(int i=values.size()-1;i>0;i--){
+        //     leftMax = max(leftMax,values[i]-i);
+        //     st.push(leftMax);
+        // }
+        // for(int i=0;i<values.size()-1;i++){
+        //     ans=max(ans,values[i]+i+st.top());
+        //     st.pop();
+        // }
+        // return ans;
+        
+        // Time - O(N) & Space - O(1) 
+        int res=0,leftMax=0;
+        for(int i=0;i<values.size();i++){
+            res = max(res,leftMax+values[i]-i);
+            leftMax = max(leftMax,values[i]+i);
         }
-        for(int i=0;i<values.size()-1;i++){
-            ans=max(ans,values[i]+i+st.top());
-            st.pop();
-        }
-        return ans;
+        return res;
     }
 };
