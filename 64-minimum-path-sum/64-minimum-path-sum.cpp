747ea387c5ea5962d89f583d725db20df4466aaa
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-01 10:08:26 +0530

    Time: 12 ms (61.14%), Space: 10.2 MB (28.47%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,16 +1,14 @@
 class Solution {
-private:
-    int t[201][201];
 public:
-    int solve(vector<vector<int>>& grid,int i, int j){
-        if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
-        if(t[i][j]!=-1) return t[i][j];
-        else if(i==grid.size()-1) return t[i][j]=grid[i][j]+solve(grid,i,j+1);
-        else if(j==grid[0].size()-1) return t[i][j]=grid[i][j]+solve(grid,i+1,j);
-        return t[i][j]=grid[i][j]+min(solve(grid,i+1,j),solve(grid,i,j+1));
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        memset(t,-1,sizeof(t));
-        return solve(grid,0,0);
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
+        dp[0][1] = 0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                dp[i][j] = grid[i-1][j-1] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 };
