6a595c2d79a7dcbe91f0f894864c4653b4d6f2bf
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-03 10:11:04 +0530

    Time: 327 ms (76.86%), Space: 124.1 MB (47.33%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,20 @@
 class Solution {
+    
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        int res=0;
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            if(manager[i]!=-1) adj[manager[i]].push_back(i);
-        }
-        queue<pair<int,int>>q;
-        q.push({headID,0});
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int node = q.front().first;
-                int time = q.front().second;
-                q.pop();
-                int dur = informTime[node]+time;
-                res = max(res,dur);
-                if(informTime[node]!=0){
-                    for(auto& j:adj[node]) q.push({j,dur});
-                }
-            }
+    int dfs(vector<vector<int>>& adj, vector<int>& time, int node){
+        if(adj[node].size()==0) return time[node];
+        int res = 0;
+        for(auto& i:adj[node]){
+             res = max(res,time[node] + dfs(adj,time,i));
         }
         return res;
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>>adj(n);
+        vector<int>vis(n,0);
+        for(int i=0;i<manager.size();i++){
+            if(manager[i]!=-1) adj[manager[i]].push_back(i);
+        }
+        return dfs(adj,informTime,headID);
     }
 };
