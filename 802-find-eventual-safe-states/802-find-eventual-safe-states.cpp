76b8a6474dab56f3c8e42ab42076ff6db1179c3d
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-03 10:39:12 +0530

    Time: 196 ms (66.32%), Space: 47.5 MB (48.53%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& adj, int node, vector<int>& vis,vector<int>& dp){
+        if(vis[node]) return false;
+        if(adj[node].size()==0) return true;
+        if(dp[node]!=-1) return dp[node];
+        vis[node] = 1;
+        bool ans = true;
+        for(auto& i:adj[node]){
+            ans = ans && dfs(adj,i,vis,dp);
+        }
+        vis[node] = 0;
+        return dp[node] = ans;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>vis(n,0),res;
+        vector<int>dp(n,-1);
+        for(int i=0;i<n;i++){
+            if(dfs(graph,i,vis,dp)) res.push_back(i);
+        }
+        return res;
+    }
+};
