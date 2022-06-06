0dfc5a957bb4086289b5507de73d725334b6c1b0
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-06 12:31:54 +0530

    Time: 423 ms (19.71%), Space: 10 MB (24.22%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>nums;
+        int num = sqrt(n),amount=n;
+        for(int i=1;i<=num;i++){
+            nums.push_back(i*i);
+        }
+        n = nums.size();
+        int dp[n+1][amount+1];
+        for(int i=0;i<amount+1;i++) dp[0][i] = INT_MAX-1;
+        for(int i=0;i<n+1;i++) dp[i][0] = 0;
+        for(int i=1;i<amount+1;i++){
+            if(i%nums[0]==0) dp[1][i] = i/nums[0];
+            else dp[1][i] = INT_MAX-1;
+        }
+        for(int i=2;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(nums[i-1]<=j) dp[i][j] = min(1+dp[i][j-nums[i-1]],dp[i-1][j]);
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][amount]!=INT_MAX-1 ? dp[n][amount]:-1;
+    }
+};
