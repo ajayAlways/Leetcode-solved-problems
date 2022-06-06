26e383ed601fb9a3c31005cf52c5147da1d6425c
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-06 12:04:46 +0530

    Time: 4 ms (28.66%), Space: 5.8 MB (90.29%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int integerBreak(int n) {
+        int res=1;
         if(n==2) return 1;
         if(n==3) return 2;
         if(n==4) return 4;
-        int prod=1;
         while(n>4){
-            prod=prod*3;
+            res=res*3;
             n=n-3;
         }
-        prod=prod*n;
-        return prod;
+        return res*n;
     }
-    
 };
