066afa0a1308fe2ec6d2d948fa8447fcba5a0d43
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-08 10:51:21 +0530

    Time: 93 ms (76.38%), Space: 29.9 MB (83.02%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,26 +2,17 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int>degree(n,0);
-        unordered_set<string>s;
+        vector<vector<int>>adj(n,vector<int>(n,0));
         int res=0;
         for(auto& i:roads){
             degree[i[0]]++;
             degree[i[1]]++;
-        }
-        for(auto& i:roads){
-            string str,rev;
-            str.append(to_string(i[0])),str.push_back('-'),str.append(to_string(i[1]));
-            rev.append(to_string(i[1])),rev.push_back('-'),rev.append(to_string(i[0]));
-            s.insert(str);
-            s.insert(rev);
+            adj[i[0]][i[1]] = 1;
+            adj[i[1]][i[0]] = 1;
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                string str,rev;
-                str.append(to_string(i)),str.push_back('-'),str.append(to_string(j));
-                rev.append(to_string(j)),rev.push_back('-'),rev.append(to_string(i));
-                
-                if(!s.count(str) || !s.count(rev)) res = max(res,degree[i]+degree[j]);
+                if(!adj[i][j] || !adj[j][i]) res = max(res,degree[i]+degree[j]);
                 else res = max(res,degree[i]+degree[j]-1);
             }
         }
