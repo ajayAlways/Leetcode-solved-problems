3c727e680c45c0ac07d68a274a88b19a75fca3f7
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-09 08:58:12 +0530

    Time: 26 ms (20.16%), Space: 15.7 MB (6.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int i=0,j=numbers.size()-1;
-        
-        while(i<j){
-            if((numbers[i]+numbers[j])==target){
-                return {i+1,j+1};
+        vector<int>res;
+        for(;i<j;){
+            if(numbers[i]+numbers[j]==target){
+                res.push_back(i+1);
+                res.push_back(j+1);
+                break;
             }
-            else if(numbers[i]+numbers[j]>target){
-                j--;
-            }
-            else if(numbers[i]+numbers[j]<target){
-                i++;
-            }
+            else if(numbers[i]+numbers[j]>target) j--;
+            else i++;
         }
-        return {-1,-1};
+        return res;
     }
 };
