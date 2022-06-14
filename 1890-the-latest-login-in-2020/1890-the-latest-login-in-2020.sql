c7518ca8f35e4d36f5f35aa3971d6466f3f95f52
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-14 13:02:15 +0530

    Time: 601 ms (68.48%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -2,5 +2,6 @@
 
 SELECT user_id, max(time_stamp) as last_stamp
 FROM Logins
-WHERE time_stamp BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59'
+# WHERE time_stamp BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59'
+WHERE YEAR(time_stamp)=2020
 GROUP BY user_id
