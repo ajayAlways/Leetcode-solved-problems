d2355a2718e92bb32b43f526d40e16edc9cf052c
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-14 14:16:15 +0530

    Time: 418 ms (95.25%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -2,9 +2,10 @@
 SELECT s1.stock_name,(s1.price-s2.price) as capital_gain_loss
 FROM (SELECT stock_name,operation,SUM(price) as price
 FROM Stocks
-GROUP BY stock_name,operation) s1,
+GROUP BY stock_name,operation) s1
+JOIN
 (SELECT stock_name,operation,SUM(price) as price
 FROM Stocks
 GROUP BY stock_name,operation) s2
 
-WHERE s1.stock_name=s2.stock_name AND s1.operation='Sell' AND s2.operation='Buy'
+ON s1.stock_name=s2.stock_name AND s1.operation='Sell' AND s2.operation='Buy'
