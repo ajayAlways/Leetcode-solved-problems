a325ffed82cc4edaeb0c85d8081c1329aa609c83
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-14 12:05:28 +0530

    Time: 2350 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
 SELECT name FROM SalesPerson
-WHERE name NOT IN  (SELECT DISTINCT SalesPerson.name AS name
-FROM SalesPerson, Company, Orders
-WHERE SalesPerson.sales_id = Orders.sales_id AND Orders.com_id=Company.com_id AND Company.name="RED")
+WHERE sales_id NOT IN  (SELECT DISTINCT sales_id
+FROM Company, Orders
+WHERE Orders.com_id=Company.com_id AND Company.name="RED")
