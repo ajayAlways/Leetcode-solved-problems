6c85f6973dbd504cf860bd5a11cd89939d064513
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-16 22:48:02 +0530

    Time: 8 ms (41.95%), Space: 11.2 MB (89.10%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    void solve(string s,int open, int close,vector<string>& res){
+    void solve(string& s,int open, int close,vector<string>& res){
         if(open==0 && close==0){
             res.push_back(s);
             return;
         }
-        if(open==close) solve(s+'(',open-1,close,res);
+        if(open==close){
+            s.push_back('(');
+            solve(s,open-1,close,res);
+            s.pop_back();
+        }
         else{
-            if(close!=0) solve(s+')',open,close-1,res);
-            if(open!=0) solve(s+'(',open-1,close,res);
+            if(close!=0){
+                s.push_back(')');
+                solve(s,open,close-1,res);
+                s.pop_back();
+            }
+            if(open!=0){
+                s.push_back('(');
+                solve(s,open-1,close,res);
+                s.pop_back();
+            }
         }
     }
     vector<string> generateParenthesis(int n) {
         vector<string>res;
-        solve("",n,n,res);
+        string s;
+        solve(s,n,n,res);
         return res;
     }
 };
