7ee05116dfa68e35043554f87d2f1271f674c34d
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-06-17 22:07:35 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (60.66%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -5,67 +5,77 @@ public:
         int n = digits.length();
         vector<string>res;
         if(n==0) return {};
-        if(n==1){
-            string s = arr[digits[0]-'0'],temp;
-            for(int i=0;i<s.length();i++){
-                temp.push_back(s[i]);
-                res.push_back(temp);
-                temp.pop_back();
-            }
-        }
-        else if(n==2){
-            string s = arr[digits[0]-'0'],temp;
-            for(int i=0;i<s.length();i++){
-                temp.push_back(s[i]);
-                string s2 = arr[digits[1]-'0'];
-                for(int j=0;j<s2.length();j++){
-                    temp.push_back(s2[j]);
-                    res.push_back(temp);
-                    temp.pop_back();
+        // if(n==1){
+        //     string s = arr[digits[0]-'0'],temp;
+        //     for(int i=0;i<s.length();i++){
+        //         temp.push_back(s[i]);
+        //         res.push_back(temp);
+        //         temp.pop_back();
+        //     }
+        // }
+        // else if(n==2){
+        //     string s = arr[digits[0]-'0'],temp;
+        //     for(int i=0;i<s.length();i++){
+        //         temp.push_back(s[i]);
+        //         string s2 = arr[digits[1]-'0'];
+        //         for(int j=0;j<s2.length();j++){
+        //             temp.push_back(s2[j]);
+        //             res.push_back(temp);
+        //             temp.pop_back();
+        //         }
+        //         temp.pop_back();
+        //     }
+        // }
+        // else if(n==3){
+        //     string s = arr[digits[0]-'0'],temp;
+        //     for(int i=0;i<s.length();i++){
+        //         temp.push_back(s[i]);
+        //         string s2 = arr[digits[1]-'0'];
+        //         for(int j=0;j<s2.length();j++){
+        //             temp.push_back(s2[j]);
+        //             string s3 = arr[digits[2]-'0'];
+        //             for(int k=0;k<s3.length();k++){
+        //                 temp.push_back(s3[k]);
+        //                 res.push_back(temp);
+        //                 temp.pop_back();
+        //             }
+        //             temp.pop_back();
+        //         }
+        //         temp.pop_back();
+        //     }
+        // }
+        // else if(n==4){
+        //     string s = arr[digits[0]-'0'],temp;
+        //     for(int i=0;i<s.length();i++){
+        //         temp.push_back(s[i]);
+        //         string s2 = arr[digits[1]-'0'];
+        //         for(int j=0;j<s2.length();j++){
+        //             temp.push_back(s2[j]);
+        //             string s3 = arr[digits[2]-'0'];
+        //             for(int k=0;k<s3.length();k++){
+        //                 temp.push_back(s3[k]);
+        //                 string s4 = arr[digits[3]-'0'];
+        //                 for(int l=0;l<s4.length();l++){
+        //                     temp.push_back(s4[l]);
+        //                     res.push_back(temp);
+        //                     temp.pop_back();
+        //                 }
+        //                 temp.pop_back();
+        //             }
+        //             temp.pop_back();
+        //         }
+        //         temp.pop_back();
+        //     }
+        // }
+        res.push_back("");
+        for(auto& digit:digits){
+            vector<string>temp;
+            for(auto& i:arr[digit-'0']){
+                for(auto& j:res){
+                    temp.push_back(j+i);
                 }
-                temp.pop_back();
             }
-        }
-        else if(n==3){
-            string s = arr[digits[0]-'0'],temp;
-            for(int i=0;i<s.length();i++){
-                temp.push_back(s[i]);
-                string s2 = arr[digits[1]-'0'];
-                for(int j=0;j<s2.length();j++){
-                    temp.push_back(s2[j]);
-                    string s3 = arr[digits[2]-'0'];
-                    for(int k=0;k<s3.length();k++){
-                        temp.push_back(s3[k]);
-                        res.push_back(temp);
-                        temp.pop_back();
-                    }
-                    temp.pop_back();
-                }
-                temp.pop_back();
-            }
-        }
-        else if(n==4){
-            string s = arr[digits[0]-'0'],temp;
-            for(int i=0;i<s.length();i++){
-                temp.push_back(s[i]);
-                string s2 = arr[digits[1]-'0'];
-                for(int j=0;j<s2.length();j++){
-                    temp.push_back(s2[j]);
-                    string s3 = arr[digits[2]-'0'];
-                    for(int k=0;k<s3.length();k++){
-                        temp.push_back(s3[k]);
-                        string s4 = arr[digits[3]-'0'];
-                        for(int l=0;l<s4.length();l++){
-                            temp.push_back(s4[l]);
-                            res.push_back(temp);
-                            temp.pop_back();
-                        }
-                        temp.pop_back();
-                    }
-                    temp.pop_back();
-                }
-                temp.pop_back();
-            }
+            res.swap(temp);
         }
         return res;
     }
