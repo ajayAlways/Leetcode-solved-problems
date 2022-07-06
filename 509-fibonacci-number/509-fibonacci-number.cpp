ecc92b64ab3e58d3bb4d92508a7b78c4629d456b
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-07-06 08:58:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
-private:
-    int t[31];
 public:
-    int solve(int n){
-        if(n<2) return n;
-        if(t[n]!=-1) return t[n];
-        return t[n] = fib(n-1)+fib(n-2);
-    }
     int fib(int n) {
-        memset(t,-1,sizeof(t));
-        return solve(n);
+        int a=1,b=0;
+        if(n<2) return n;
+        for(int i=2;i<=n;i++){
+            int temp = a;
+            a=a+b;
+            b=temp;
+        }
+        return a;
     }
 };
