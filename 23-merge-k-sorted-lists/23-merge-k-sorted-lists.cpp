3672cb7c2e79f07c868d398c05b208af35a5ed25
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-07-20 12:07:33 +0530

    Time: 29 ms (79.45%), Space: 13.4 MB (46.99%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,28 +11,45 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        if(n==0) return NULL;
-        if(n==1 && !lists[0]) return NULL;
-        while(lists.size()>1){
-            ListNode *dummy = new ListNode(),*root=dummy,*l1=lists[lists.size()-1],*l2=lists[lists.size()-2];
-            while(l1 && l2){
-                if(l1->val < l2->val){
-                    root->next = l1;
-                    l1 = l1->next;
-                }
-                else{
-                    root->next = l2;
-                    l2 = l2->next;
-                }
-                root = root->next;
-            }
-            if(l1) root->next = l1;
-            if(l2) root->next = l2;
-            lists.pop_back();
-            lists.pop_back();
-            lists.push_back(dummy->next);
+        // TC - O(n^2);
+        // int n = lists.size();
+        // if(n==0) return NULL;
+        // if(n==1 && !lists[0]) return NULL;
+        // while(lists.size()>1){
+        //     ListNode *dummy = new ListNode(),*root=dummy,*l1=lists[lists.size()-1],*l2=lists[lists.size()-2];
+        //     while(l1 && l2){
+        //         if(l1->val < l2->val){
+        //             root->next = l1;
+        //             l1 = l1->next;
+        //         }
+        //         else{
+        //             root->next = l2;
+        //             l2 = l2->next;
+        //         }
+        //         root = root->next;
+        //     }
+        //     if(l1) root->next = l1;
+        //     if(l2) root->next = l2;
+        //     lists.pop_back();
+        //     lists.pop_back();
+        //     lists.push_back(dummy->next);
+        // }
+        // return lists[0];
+        
+            priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>minHeap;
+        ListNode *dummy = new ListNode(), *head = dummy;
+        for(auto& i:lists){
+            if(i!=NULL) minHeap.push({i->val,i});
         }
-        return lists[0];
+        while(!minHeap.empty()){
+            ListNode* node = minHeap.top().second;
+            minHeap.pop();
+            head->next = node;
+            head = head->next;
+            if(node->next) minHeap.push({node->next->val,node->next});
+        }
+        head = dummy->next;
+        delete(dummy);
+        return head;
     }
 };
