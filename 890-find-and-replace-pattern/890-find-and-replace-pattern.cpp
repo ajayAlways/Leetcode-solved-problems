195946447587d7524348eec0b75428301ab329d5
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-07-29 11:51:51 +0530

    Time: 10 ms (28.33%), Space: 7.9 MB (92.07%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        vector<string>ans;
+        vector<char>alpha(26,'0');
+        vector<string>res;
+        vector<char>map(26,'0');
         for(int i=0;i<words.size();i++){
-            vector<int>v(26,-1);
-            unordered_set<int>ss;
-            int flag=1;
+            int flag = 1;
+            alpha = vector<char>(26,'0');
+            map = vector<char>(26,'0');
             for(int j=0;j<pattern.length();j++){
-                if(v[pattern[j]-'a']==-1 && ss.find(words[i][j]-'a')==ss.end()){
-                    v[pattern[j]-'a'] = words[i][j]-'a';
-                    ss.insert(words[i][j]-'a');
+                if(map[words[i][j]-'a']!='0' && map[words[i][j]-'a']!=pattern[j]){
+                    flag = 0;
+                    break;
+                }
+                if(alpha[pattern[j]-'a']=='0'){
+                    alpha[pattern[j]-'a'] = words[i][j];
+                    map[words[i][j]-'a'] = pattern[j];
                 }
                 else{
-                    if(v[pattern[j]-'a'] != words[i][j]-'a') flag=0;
+                    if(alpha[pattern[j]-'a']!=words[i][j]){
+                        flag = 0;
+                        break;
+                    }
                 }
             }
-            if(flag) ans.push_back(words[i]);
+            if(flag) res.push_back(words[i]);
         }
-        return ans;
+        return res;
     }
 };
