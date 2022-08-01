8274f19da94a920fb6360ca82d7b76773bf6a254
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-01 11:43:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,7 +4,7 @@ public:
         int dp[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0) dp[i][j]=1;
+                if(i==0 || j==0) dp[i][j] = 1;
                 else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
