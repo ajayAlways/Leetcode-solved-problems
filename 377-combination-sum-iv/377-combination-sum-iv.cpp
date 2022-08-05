dba526d3be01b0cd3bf18805dc127b3a25b2c0b1
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-05 10:22:23 +0530

    Time: 4 ms (46.34%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,23 +3,33 @@ private:
     int memo[1001];
 public:
     
-    int solve(vector<int>&nums,int target, int sum){
-        if(sum==target){
-            return 1;
-        }
-        if(memo[sum]!=-1) return memo[sum];
-        int res=0;
-        for(int i=0;i<nums.size();i++){
-            if(sum+nums[i]<=target) res+=solve(nums,target,sum+nums[i]);
-        }
-        return memo[sum] = res;
-    }
+    // int solve(vector<int>&nums,int target, int sum){
+    //     if(sum==target){
+    //         return 1;
+    //     }
+    //     if(memo[sum]!=-1) return memo[sum];
+    //     int res=0;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(sum+nums[i]<=target) res+=solve(nums,target,sum+nums[i]);
+    //     }
+    //     return memo[sum] = res;
+    // }
     
     int combinationSum4(vector<int>& nums, int target) {
-        int res=0;
-        memset(memo,-1,sizeof memo);
-        res = solve(nums,target,0);
+        // int res=0;
+        // memset(memo,-1,sizeof memo);
+        // res = solve(nums,target,0);
+        int n = nums.size();
+        vector<unsigned int>dp(target+1,0);
+        sort(nums.begin(),nums.end());
+        dp[0] = 1;
+        for(int i=1;i<=target;i++){
+            for(auto& num:nums){
+                if(num>i) break;
+                dp[i]+=dp[i-num];
+            }
+        }
         
-        return res;
+        return dp[target];
     }
 };
