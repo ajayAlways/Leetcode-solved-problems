fb8801f187dd0d941654f9050f0d6428bf2296c4
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-08 19:22:42 +0530

    Time: 13 ms (86.99%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -13,14 +13,25 @@ public:
 //     }
     
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(),res=1;
-        vector<int>dp(n,1);
-        // return solve(nums,0,-1);
+        // int n = nums.size(),res=1;
+        // vector<int>dp(n,1);
+        // // return solve(nums,0,-1);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[i]>nums[j]) dp[i] = max(dp[i],1+dp[j]), res = max(res,dp[i]);
+        //     }
+        // }
+        // return res;
+        //     O(NlogN) - TC
+        int n = nums.size();
+        vector<int>res;
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]) dp[i] = max(dp[i],1+dp[j]), res = max(res,dp[i]);
-            }
+            auto it = lower_bound(res.begin(),res.end(),nums[i]);
+            if(it==res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
         }
-        return res;
+
+        return res.size();
     }
+    
 };
