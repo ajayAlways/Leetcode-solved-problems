f05555c80ec1862dcd9b72a1ce869b39ed56ed11
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-08 17:02:36 +0530

    Time: 513 ms (34.79%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,22 +1,26 @@
 class Solution {
-private:
-    int memo[2501];
-    
 public:
     
-    int solve(vector<int>& nums, int cur, int prev){
-        if(cur>=nums.size()) return 0;
-        if(memo[prev+1]!=-1) return memo[prev+1];
-        int take = 0,doNotTake = solve(nums,cur+1,prev);
-        if(prev==-1 || nums[cur]>nums[prev]){
-            take = 1+solve(nums,cur+1,cur);
-        }
+//     int solve(vector<int>& nums, int cur, int prev){
+//         if(cur>=nums.size()) return 0;
+//         if(memo[prev+1]!=-1) return memo[prev+1];
+//         int take = 0,doNotTake = solve(nums,cur+1,prev);
+//         if(prev==-1 || nums[cur]>nums[prev]){
+//             take = 1+solve(nums,cur+1,cur);
+//         }
         
-        return memo[prev+1] = max(take,doNotTake);
-    }
+//         return memo[prev+1] = max(take,doNotTake);
+//     }
     
     int lengthOfLIS(vector<int>& nums) {
-        memset(memo,-1,sizeof memo);
-        return solve(nums,0,-1);
+        int n = nums.size(),res=1;
+        vector<int>dp(n,1);
+        // return solve(nums,0,-1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]) dp[i] = max(dp[i],1+dp[j]), res = max(res,dp[i]);
+            }
+        }
+        return res;
     }
 };
