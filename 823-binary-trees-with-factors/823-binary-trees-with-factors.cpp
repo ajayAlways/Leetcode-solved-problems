936c8434a5217d101e6bb8072fc7f076cf1badaf
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-09 10:51:02 +0530

    Time: 94 ms (68.00%), Space: 9.2 MB (81.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -4,22 +4,21 @@ public:
         int n = arr.size(),mod=1e9+7;
         long long res=0;
         sort(arr.begin(),arr.end());
-        vector<long>dp(n,1);
-        unordered_map<int,int>m;
-        for(int i=0;i<n;i++) m[arr[i]] = i;
+        unordered_map<int,long>dp;
+        for(int i=0;i<n;i++) dp[arr[i]] = 1;
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+            for(int j=0;j<i;j++){
                 if(arr[i]%arr[j]==0){
-                    if(m.find(arr[i]/arr[j])!=m.end()){
-                        long left = dp[m[arr[j]]];
-                        long right = dp[m[arr[i]/arr[j]]];
-                        dp[i] = (dp[i]+left*right)%mod;
+                    if(dp.find(arr[i]/arr[j])!=dp.end()){
+                        long left = dp[arr[j]];
+                        long right = dp[arr[i]/arr[j]];
+                        dp[arr[i]] = (dp[arr[i]]+left*right)%mod;
                     }
                 }
             }
         }
         for(auto& i:dp){
-            res = (res+i)%mod;
+            res = (res+i.second)%mod;
         }
         
         return res;
