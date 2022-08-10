cb571665abf0eb915d3f3ccb994358d850b7ad3f
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-10 16:06:22 +0530

    Time: 2092 ms (5.00%), Space: 51.2 MB (8.08%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,26 +1,16 @@
 class Solution {
-// private:
-//     bool t[10001];
 public:
-    // bool solve(vector<int>& nums, int n, int i){
-    //     //BC
-    //     if(i>=n) return true;
-    //     if(t[i]==true) return true;
-    //     int reach = i+nums[i];
-    //     for(int k=i+1;k<=reach;k++){
-    //         if(solve(nums,n,k)) return t[i] = true;
-    //     }
-    //     return t[i]=false;
-    // }
-    bool canJump(vector<int>& nums) {
-        // memset(t,false,sizeof(t));
-        // return solve(nums,nums.size()-1,0);
-        int minjump=0;
-        for(int i=nums.size()-2;i>=0;i--){
-            minjump++;
-            if(nums[i]>=minjump) minjump = 0;
-            
+    bool solve(vector<int>& nums, int i, vector<int>&dp){
+        if(i>=nums.size()-1) return true;
+        if(dp[i]!=-1) return dp[i];
+        for(int pos=1;pos<=nums[i];pos++){
+            if(solve(nums,i+pos,dp)) return dp[i] = true;
         }
-        return minjump==0;
+        
+        return dp[i] = false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return solve(nums,0,dp);
     }
 };
