ed3fb1837c11864e13b5122195bcdfe048fa2400
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-10 17:58:11 +0530

    Time: 754 ms (18.84%), Space: 11.3 MB (60.78%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,39 +1,40 @@
 class Solution {
+private:
+    int memo[1001][1001];
 public:
+    
+    bool isPalin(string &s, int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    bool solve(string &s, int i, int j, int &start, int &end){
+        if(i>j) return false;
+        if(memo[i][j]!=-1) return memo[i][j];
+        if(isPalin(s,i,j)){
+            int maxLen = end-start + 1, curLen = j-i+1;
+            if(curLen>=maxLen){
+                start = i;
+                end = j;
+            }
+            return memo[i][j] = true;
+        }
+        else{
+            bool temp1 = solve(s,i+1,j,start,end);
+            bool temp2 = solve(s,i,j-1,start,end);
+            return memo[i][j] = temp1 || temp2;
+        }
+        
+    }
+    
     string longestPalindrome(string s) {
-        vector<int>v(s.length(),0);
-        int max=0,pos=0;
-        if(s.length()==1){
-            return s;
-        }
-        if(s.length()==2){
-            if(s[0]==s[1]){
-                return s;
-            }
-            else{
-                return s.substr(0,1);
-            }
-        }
-        for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>i;j--){
-                int t=i,p=j,l=j;
-                while(s[t]==s[p]&&p>=t){
-                    t++;
-                    p--;
-                }
-                if(p<t){
-                    v[i]=l-i+1;
-                    break;
-                }
-            }
-           if(v[i]>max){
-               max=v[i];
-               pos=i;
-           }
-        }
-        if(max==0){
-            return s.substr(0,1);
-        }
-        return s.substr(pos,max);
+        int start = -1, end = -1;
+        memset(memo, -1 ,sizeof memo);
+        int len = solve(s,0,s.length()-1,start,end);
+        return s.substr(start, end-start+1);
     }
 };
