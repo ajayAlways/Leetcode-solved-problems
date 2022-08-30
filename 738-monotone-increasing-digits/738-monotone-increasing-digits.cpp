010ee3131a9643ba4b64dfe43e4c641916e34311
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-30 15:22:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.75%) - LeetHub

--- 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
+++ 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
@@ -4,13 +4,15 @@ public:
     int monotoneIncreasingDigits(int n) {
         string num = to_string(n);
         int len = num.size();
-        for(int i=0;i<len-1;i++){
-            if(num[i]>num[i+1]){
-                num[i] = ((num[i]-'0')-1)+'0';
-                for(int j=i+1;j<len;j++) num[j] = '9';
-                i=-1;
+        int start=len;
+        for(int i=len-1;i>0;i--){
+            if(num[i-1]>num[i]){
+                num[i-1] = ((num[i-1]-'0')-1)+'0';
+                start = i;
             }
         }
+        for(int j=start;j<len;j++) num[j] = '9';
+        
         return stoi(num);
     }
 };
