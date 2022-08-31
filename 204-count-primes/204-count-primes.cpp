062887f53eb3005610818f8676d854d8fed8ea68
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-31 12:34:18 +0530

    Time: 360 ms (39.83%), Space: 130.5 MB (7.36%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        if(n<=2) return 0;
         vector<int>primes(n,0);
-        int res=0;
-        for(int i=2;i<n;i++){
+        int res=1,upper=sqrt(n);
+        for(int i=3;i<n;i+=2){
             if(primes[i]!=1){
                 res++;
-                for(int j=2*i;j<n;j+=i) primes[j] = 1;
+                if(i>upper) continue;
+                for(int j=i*i;j<n;j+=i*2) primes[j] = 1;
             }
         }
         
