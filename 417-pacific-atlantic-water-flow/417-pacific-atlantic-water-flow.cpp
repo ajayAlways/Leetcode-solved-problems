df6bccf27cada021b49833fb667ea6e655e13d66
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-08-31 11:47:13 +0530

    Time: 76 ms (49.49%), Space: 19 MB (49.23%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& heights,vector<vector<int>>& visited,int i, int j){
-        if(i<0 || j<0 || i>=heights.size() || j>=heights[0].size() || visited[i][j]==1) return;
-        visited[i][j] = 1;
-        if(i+1<heights.size() && heights[i][j]<=heights[i+1][j]) dfs(heights,visited,i+1,j);
-        if(i>0 && heights[i][j]<=heights[i-1][j]) dfs(heights,visited,i-1,j);
-        if(j+1<heights[0].size() && heights[i][j]<=heights[i][j+1]) dfs(heights,visited,i,j+1);
-        if(j>0 && heights[i][j]<=heights[i][j-1]) dfs(heights,visited,i,j-1);
-
+    
+    void dfs(vector<vector<int>>&h,int i, int j, vector<vector<int>>&vis){
+        if(i<0 || j<0 || i>=h.size() || j>=h[0].size() || vis[i][j]==1) return;
+        
+        vis[i][j] = 1;
+        
+        if(i+1<h.size() && h[i][j]<=h[i+1][j]) dfs(h,i+1,j,vis);
+        if(j+1<h[0].size() && h[i][j]<=h[i][j+1]) dfs(h,i,j+1,vis);
+        if(i>0 && h[i][j]<=h[i-1][j]) dfs(h,i-1,j,vis);
+        if(j>0 && h[i][j]<=h[i][j-1]) dfs(h,i,j-1,vis);
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size(), n = heights[0].size();
-        vector<vector<int>>atlantic(m,vector<int>(n,0));
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
+        int m = h.size(), n = h[0].size();
         vector<vector<int>>pacific(m,vector<int>(n,0));
-        vector<vector<int>>ans;
-        for(int i=0;i<m;i++) dfs(heights,pacific,i,0),dfs(heights,atlantic,i,n-1);
-        for(int i=0;i<n;i++) dfs(heights,pacific,0,i),dfs(heights,atlantic,m-1,i);
+        vector<vector<int>>atlantic(m,vector<int>(n,0));
+        vector<vector<int>>res;
+        
+        for(int i=0;i<m;i++) dfs(h,i,0,pacific), dfs(h,i,n-1,atlantic);
+        for(int i=0;i<n;i++) dfs(h,0,i,pacific), dfs(h,m-1,i,atlantic);
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(pacific[i][j]==1 && atlantic[i][j]==1) ans.push_back({i,j});
+                if(pacific[i][j]==1 && atlantic[i][j]==1) res.push_back({i,j});
             }
         }
-        return ans;
+        
+        return res;
     }
 };
