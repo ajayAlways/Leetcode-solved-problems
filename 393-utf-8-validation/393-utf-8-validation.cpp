43e4afb7e9728c3d13d2b76d50100b8d520ab9eb
Ajay <54075270+Ajay-creator@users.noreply.github.com>
2022-09-13 16:05:12 +0530

    Time: 53 ms (7.48%), Space: 14.1 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,32 @@
+#include<bitset>
+
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int n = data.size(), i = 0;
+        while(i<n){
+            bitset<8>bits(data[i]);
+            int j=7,cnt=0;
+            while(j>=0 && bits[j]==1){
+                j--;
+                cnt++;
+            }
+            if(!cnt){
+                i++;
+                continue;
+            }
+            if(cnt==1 || cnt>4) return false;
+            while(cnt>1){
+                i++;
+                if(i>=n) return false;
+                int t = (data[i]>>7 & 1)*10 + (data[i]>>6 & 1);
+                cout<<t<<endl;
+                if(t!=10) return false;
+                cnt--;
+            }
+            i++;
+        }
+        
+        return true;
+    }
+};
